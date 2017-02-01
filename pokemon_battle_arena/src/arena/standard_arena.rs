d07eef6b4a6d5aad76247c2ac5d63b8aa714e115
MatthiasThien <matthias.thien@web.de>
2017-02-01 02:34:53 +0100

    added my ideas in comments for discussion

--- pokemon_battle_arena/src/arena/standard_arena.rs
+++ pokemon_battle_arena/src/arena/standard_arena.rs
@@ -2,31 +2,53 @@ use player::Player;
 use db::pokemon_token::PokemonToken;
 use db::enums;
 
-/// The standard arena is based on the default 1v1 fight.
+/// The standard arena is based on the default XvX fight.
 
-pub fn fight(mut player_one: &Player, mut player_two: &Player) {
+impl super::Arena {
+    pub fn fight(&self) {
 
-    let mut p1_pokemon = &player_one.get_pokemon_list();
-    let mut p2_pokemon = &player_two.get_pokemon_list();
-    let mut p1_current = 0;
-    let mut p2_current = 0;
+        // Move der Spieler von Team 1 abfragen. (Methode in Player?)
+        // Move der Spieler von Team 2 abfragen. (Methode in Player?)
+            // Mögliche Moves:
+                // Pokemon Tausch (Swap)
+                // Attacke wählen
+            // Auswahlscreens müssen für jeden human zur verfügung gestellt werden.
+            // 1. Screen: Auswahl zwischen Swap und Attacke
+            // Swap Screen: Anzeigen der Pokemon mit HP und Status (Schlaf etc.) evtl. more infos
+            //              Button. Back Button
+            // Attack Screen: Auswahl der Attacken mit AP und Typ(Farbig hinterlegt?) Back Button
+
+        // Wenn ein Spieler sein Pokemon tauschen möchte wird dies zu erst gemacht. Bei mehreren
+        // in der Reihenfolge T1[0,1,2...] dann T2[0,1,2...]
 
 
+        // Entscheidung zwischen Attackentypen:
+            // Prioattacken zu erst (Ruckzuckhieb z.B.)
+            // Dann Normale Attacken mit init wert
 
-    loop {
-        if p1_pokemon[p1_current].get_current().get_stat(enums::Stats::Speed) >=
-            p2_pokemon[p2_current].get_current().get_stat(enums::Stats::Speed) {
-            // pokemon from p1 is faster and starts
-            battle(&p1_pokemon[p1_current], &p2_pokemon[p2_current]);
-        } else {
-            // pokemon from p2 is faster and starts
-            battle(&p2_pokemon[p2_current], &p1_pokemon[p1_current]);
-        }
+
+        // Diese methode wird die Moves der einzelnen Spieler abfragen und diese abspeichern.
+        // Anschließend wird von hier aus in der jeweiligen Reihenfolge swap und battle aufgerufen.
+
+
+        // Von hier müsste auch die UI gesteuert werden. Zumindest in meinen Augen
     }
 }
 
-/// Simulates one round in a fight
-/// Every given pokemon doing one action.
-fn battle(pokemon_one: &PokemonToken, pokemon_two: &PokemonToken) {
+fn swap() {
+    // von hier aus wird der tausch des aktiven pokemon abgehandelt.
+}
 
+
+fn battle(pokemon_one: &PokemonToken, pokemon_two: &PokemonToken) {
+    // Hier wird zunächst auf die Priorität der Attacken und dann auf den Init Wert der pokemon
+    // geprüft. Dann wird jeweils math aufgerufen.
+    // Noch unklar: Schlaf, Paralyse, Vernarrtheit, etc.
+    // XvX Kampf mit Area Attack wie Surfer: Attacke als Attacke gegen ein Pokemon ansehen und math
+    // öfters aufrufen?
+}
+
+fn math<T>(pokemon_one: &PokemonToken, pokemon_two: &PokemonToken) {
+    // Verändert die Stats des Angreifenden und des Verteidigenden Pkmn nach angegebener Attacke
+    // Wetterverhältnisse und typ der arena werden hier miteinbezogen
 }
