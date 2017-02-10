36933f75fce0b9edb0eff4e907b3b2f6899f6b0b
Luka Vukusic <Luka Vukusic>
2017-02-10 22:46:31 +0100

    Changed the team list's implementation to an actual list

--- pokemon_battle_arena/src/graphic/gui.rs
+++ pokemon_battle_arena/src/graphic/gui.rs
@@ -79,7 +79,7 @@ pub fn draw_window() {
     let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
 
     // Create Ids for every used widget
-    let mut ids = Ids::new(ui.widget_id_generator());
+    let ids = Ids::new(ui.widget_id_generator());
 
     // Add a font from file
     let assets = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap_or_else(
@@ -98,9 +98,6 @@ pub fn draw_window() {
 
     let mut app = App::new();
 
-    // List of selections, should be same length as list of entries. Will be updated by the widget.
-    let mut list_selected: ::std::collections::HashSet<usize> = ::std::collections::HashSet::new();
-
     // Poll events from the window.
     while let Some(event) = window.next_event(&mut events) {
         // Convert the piston event to a conrod event.
@@ -117,6 +114,9 @@ pub fn draw_window() {
             widget::Canvas::new().color(app.bg_color).set(ids.canvas, ui);
 
             // draws Title-Screen
+            // Contains:    Play-Button
+            //              Options-Button
+            //              Exit-Button
             if let Screen::Title = app.screen {
                 // Play button
                 // Shows Play-Screen when clicked
@@ -234,45 +234,55 @@ pub fn draw_window() {
             // Draws Choose Team Screen
             // IDEAS:   - pictures of pokemon
             //          - filter gen/type (get pokedex with only specific type/gen)
-            //          - search ( fn that gives Vec with all pokemon that have certain string in name)
+            //          - search ( fn that gives Vec with pokemon that have certain string in name)
             if let Screen::ChooseTeam = app.screen {
+
+                // ===============================================================
+                // = Scrollable List from which the Player can select their team =
+                // ===============================================================
                 let pokedex_entries = app.pokedex.get_entries();
                 let num_items = pokedex_entries.len();
                 let item_h = 32.0;
+
                 let (mut events, scrollbar) = widget::ListSelect::single(num_items, item_h)
                     .scrollbar_next_to()
                     .w_h(200.0, 650.0)
                     .mid_left_with_margin_on(ids.canvas, 25.0)
                     .scrollbar_width(15.0)
-                    .set(ids.s_list_pokemon, ui);
+                    .set(ids.s_list_pkmn, ui);
 
+                // Instantiate the scrollbar for the list.
+                if let Some(s) = scrollbar { s.set(ui); }
+
+                // Create Vec with all the pokemon names to be displayed
                 let mut pkmn_names = Vec::new();
                 for pokemon in pokedex_entries {
                     pkmn_names.push(pokemon.get_name());
                 }
 
                 // Handle the `ListSelect`s events.
-                while let Some(event) = events.next(ui, |i| list_selected.contains(&i)) {
+                while let Some(event) = events.next(ui, |i| ::std::collections::HashSet::<usize>::new().contains(&i)) {
                     use conrod::widget::list_select::Event;
+
                     match event {
                         // For the `Item` events we instantiate the `List`'s items.
                         Event::Item(item) => {
                             let label = &pkmn_names[item.i];
-                            let font_size = item_h as conrod::FontSize / 2;
 
+                            // each button with the respective pokemon name as label
                             let button = widget::Button::new()
                                 .border(0.0)
                                 .color(conrod::color::LIGHT_GREY)
                                 .label(label)
-                                .label_font_size(font_size)
                                 .label_color(app.label_color);
                             item.set(button, ui);
                         },
 
-                        // The selection has changed.
+                        // Add pokemon to team when button is pressed
                         Event::Selection(selection) => {
-                            println!("selected index: {:?}", selection);
+                            println!("selected index Pokemon: {:?}", selection);
 
+                            // as long as the team is not full add new pokemon...
                             if app.pkmn_team.len() < 6 {
                                 app.pkmn_team.push(
                                     db::pokemon_token::PokemonToken::from_model(
@@ -285,178 +295,75 @@ pub fn draw_window() {
                                 for i in 0..app.pkmn_team.len() {
                                     println!("{:?}", app.pkmn_team[i].get_name());
                                 }
+                            // ...else give message that team is full
                             } else {
                                 println!("Error: only 6 pokemon per team allowed");
                             }
                         }
-
-                        // The remaining events indicate interactions with the `ListSelect` widget.
                         _ => {},
                     }
                 }
 
-                // Instantiate the scrollbar for the list.
-                if let Some(s) = scrollbar { s.set(ui); }
+                // =====================================
+                // = List that shows the player's team =
+                // =====================================
+                let (mut events, _) = widget::ListSelect::single(6, 650.0/6.0)
+                    .w_h(200.0, 650.0)
+                    .mid_right_with_margin_on(ids.canvas, 25.0)
+                    .set(ids.s_list_team, ui);
 
-                // ***** Buttons mit ausgewählten Pokemon *****
-                // TODO/IDEE : makro für schönere/kürzere implementierung
-                if app.pkmn_team.len() > 0 {
-                    let label_p1 = app.pkmn_team[0].get_name();
-                    if widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label(&label_p1)
-                        .label_color(app.label_color)
-                        .top_right_with_margin_on(ids.canvas, 25.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p1, ui)
-                        .was_clicked()
-                    {
-                        println!("p1");
-                        app.pkmn_team.remove(0);
-                    }
-                } else {
-                    widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label("-")
-                        .label_color(app.label_color)
-                        .top_right_with_margin_on(ids.canvas, 25.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p1, ui);
+                let mut team_names = Vec::with_capacity(6);
+                for pokemon in &app.pkmn_team {
+                    team_names.push(pokemon.get_name());
                 }
 
-                if app.pkmn_team.len() > 1 {
-                    let label_p2 = app.pkmn_team[1].get_name();
-                    if widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label(&label_p2)
-                        .label_color(app.label_color)
-                        .down_from(ids.button_p1, 0.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p2, ui)
-                        .was_clicked()
-                    {
-                        println!("p2");
-                        app.pkmn_team.remove(1);
+
+                // Handle the `ListSelect`s events.
+                while let Some(event) = events.next(ui, |i| ::std::collections::HashSet::<usize>::new().contains(&i)) {
+                    use conrod::widget::list_select::Event;
+
+                    match event {
+                        // For the `Item` events we instantiate the `List`'s items.
+                        Event::Item(item) => {
+                            // `label` is either the name of the pokemon (if available)
+                            // or - (if not)
+                            let label = if &team_names.len() > &item.i {
+                                &team_names[item.i]
+                            } else {
+                                "-"
+                            };
+
+                            // each list item is a button with `label` as label
+                            let button = widget::Button::new()
+                                .border(0.0)
+                                .color(conrod::color::LIGHT_GREY)
+                                .label(label)
+                                .label_color(app.label_color);
+                            item.set(button, ui);
+                        },
+
+                        // Remove pokemon from team when button is pressed
+                        //
+                        // TODO:
+                        //  Change functionality such that the selected pokemon is displayed
+                        //  in the middle of the screen (with stats, etc)
+                        Event::Selection(selection) => {
+                            println!("selected index Team: {:?}", selection);
+                            app.pkmn_team.remove(selection);
+                        }
+                        // Do nothing for every other event
+                        _ => {},
                     }
-                } else {
-                    widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label("-")
-                        .label_color(app.label_color)
-                        .down_from(ids.button_p1, 0.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p2, ui);
                 }
 
-                if app.pkmn_team.len() > 2 {
-                    let label_p3 = app.pkmn_team[2].get_name();
-                    if widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label(&label_p3)
-                        .label_color(app.label_color)
-                        .down_from(ids.button_p2, 0.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p3, ui)
-                        .was_clicked()
-                    {
-                        println!("p3");
-                        app.pkmn_team.remove(2);
-                    }
-                } else {
-                    widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label("-")
-                        .label_color(app.label_color)
-                        .down_from(ids.button_p2, 0.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p3, ui);
-                }
+                let stats = "
+                    ======= TEST =======\n\n
 
-                if app.pkmn_team.len() > 3 {
-                    let label_p4 = app.pkmn_team[3].get_name();
-                    if widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label(&label_p4)
-                        .label_color(app.label_color)
-                        .down_from(ids.button_p3, 0.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p4, ui)
-                        .was_clicked()
-                    {
-                        println!("p4");
-                        app.pkmn_team.remove(3);
-                    }
-                } else {
-                    widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label("-")
-                        .label_color(app.label_color)
-                        .down_from(ids.button_p3, 0.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p4, ui);
-                }
-
-                if app.pkmn_team.len() > 4 {
-                    let label_p5 = app.pkmn_team[4].get_name();
-                    if widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label(&label_p5)
-                        .label_color(app.label_color)
-                        .down_from(ids.button_p4, 0.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p5, ui)
-                        .was_clicked()
-                    {
-                        println!("p5");
-                        app.pkmn_team.remove(4);
-                    }
-                } else {
-                    widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label("-")
-                        .label_color(app.label_color)
-                        .down_from(ids.button_p4, 0.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p5, ui);
-                }
-
-                if app.pkmn_team.len() > 5 {
-                    let label_p6 = app.pkmn_team[5].get_name();
-                    if widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label(&label_p6)
-                        .label_color(app.label_color)
-                        .down_from(ids.button_p5, 0.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p6, ui)
-                        .was_clicked()
-                    {
-                        println!("p6");
-                        app.pkmn_team.remove(5);
-                    }
-                } else {
-                    widget::Button::new()
-                        .border(1.0)
-                        .color(app.bg_color)
-                        .label("-")
-                        .label_color(app.label_color)
-                        .down_from(ids.button_p5, 0.0)
-                        .w_h(BUTTON_W, 650.0/6.0)
-                        .set(ids.button_p6, ui);
-                }
-
-                let stats = "TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST ";
+                     Screen that shows:\n
+                      - Sprite
+                      - Stats
+                      - Attacks
+                ";
 
                 widget::Text::new(stats)
                     .color(app.label_color)
@@ -476,7 +383,7 @@ pub fn draw_window() {
                     .was_clicked()
                 {
                     println!("Back");
-                    app.screen = Screen::Title;
+                    app.screen = Screen::Play;
                 }
 
                 if widget::Button::new()
@@ -520,16 +427,11 @@ widget_ids! {
         button_sp,
         button_mp,
         button_back,
-        button_p1,
-        button_p2,
-        button_p3,
-        button_p4,
-        button_p5,
-        button_p6,
         button_fight,
 
         // *** selection_list ***
-        s_list_pokemon,
+        s_list_pkmn,
+        s_list_team,
 
         // *** text ***
         text_sel_pkmn,
